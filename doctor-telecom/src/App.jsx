e15d0b8b78a10fa5426ee8aa3b7b7295ff31e310
Hayden Dinkin <hjdinkin@gmail.com>
2024-11-09 17:56:29 -0500

    style updates

--- doctor-telecom/src/App.jsx
+++ doctor-telecom/src/App.jsx
@@ -9,39 +9,11 @@ import Navigation from './Navigation.jsx';
 import { BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const message = "What's your purpose?";
-  const answer = Chatbot(message);
-  console.log(answer);
-
   return (
     <>
       <Router>
       <Navbar/>
-      <Navigation/>
-      <div className="main-content">
-        
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
+      <Chatbot/>
       </Router>
     </>
   )

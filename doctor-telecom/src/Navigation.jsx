377e1f3200158a39208fd4b966c71bdd6b5f4acb
Rittivuth Chea <rittivuthchea@cog-13.dynamic2.rpi.edu>
2024-11-09 14:16:27 -0500

    added navbar

--- doctor-telecom/src/Navigation.jsx
+++ doctor-telecom/src/Navigation.jsx
@@ -1,4 +1,5 @@
 import Tab from './Tab.jsx';
+import './navbar.css';
 
 function Navigation() {
     return (
@@ -6,7 +7,24 @@ function Navigation() {
             <h1>Navigation Bar</h1>
             <Tab value="Diagonsis Help"/>
             <Tab value="Connect with Doctor"/>
+
+            <div className="navbar">
+            <h1>My App</h1>
+            <div className="tab">
+            <a href="#tab1">Tab 1</a>
+            </div>
+            <div className="tab">
+            <a href="#tab2">Tab 2</a>
+            </div>
+            <div className="tab">
+            <a href="#tab3">Tab 3</a>
+            </div>
+            <div className="tab">
+            <a href="#tab4">Tab 4</a>
+            </div>
+            </div>
         </>
+        
     );
 }
 
